aa6b511a0c9112d8017499d57ceec3ae6a63a878
DeanoLark <deanolark@gmail.com>
2023-12-31 21:40:24 +0000

    First commit

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+import FeedbackItem from "./FeedbackItem"
+
+function FeedbackList({ feedback, handleDelete }) {
+    if (!feedback || feedback.length === 0) {
+        return 'No Feedback Yet'
+    }
+
+    return <div className="feedback-list">
+        {feedback.map((item) => {
+            return <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+        })}
+    </div>
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.array.isRequired,
+}
+
+export default FeedbackList
